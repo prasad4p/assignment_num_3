19af05dc92c528e84d7bfa49272249729d67b46f
prasad4p <122027371+prasad4p@users.noreply.github.com>
2023-01-29 20:34:15 +0530

    Question_num_2

---
+++ _3_assignment_question_num_2.py
@@ -0,0 +1,14 @@
+# Assignment_number_3
+# Question_number_2 = Write a Python program to reverse a string.
+
+
+
+def reverse():
+
+    str = input ("Enter the string : ")
+    reverse_string = str [::-1]
+
+    print ("Reverse of string  : ", reverse_string)
+    
+reverse()
+
